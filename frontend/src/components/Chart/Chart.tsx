e4f591d03a7cf05e58ab6aaa831eaad6f7653cea
Rettera <trof1m00@mail.ru>
2022-07-23 02:25:00 +0300

    fix: Добавить информацию по числу заказов и итоговой стоимости

--- frontend/src/components/Chart/Chart.tsx
+++ frontend/src/components/Chart/Chart.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Typography from '@mui/material/Typography';
 import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import styles from './Chart.module.css';
 
@@ -19,12 +20,18 @@ const CustomTooltip = ({ active, payload, label }) => {
 
 export default function Chart() {
   const [stats, setStats] = useState([]);
+  const [total_orders, setTotalOrders] = useState([]);
+  const [total_cost_usd, setTotalCostUSD] = useState([]);
+  const [total_cost_rub, setTotalCostRUB] = useState([]);
   
   function load_statistics() {
     fetch('http://127.0.0.1:5000/get_stats')
     .then((response) => response.json())
     .then((data) => {
         setStats(data.Stats);
+        setTotalOrders(data.TotalOrders);
+        setTotalCostUSD(data.TotalCostUSD);
+        setTotalCostRUB(data.TotalCostRUB);
     });
   }
 
@@ -33,23 +40,28 @@ export default function Chart() {
   }, []);
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <AreaChart
-        data={stats}
-        margin={{
-          top: 16,
-          right: 16,
-          bottom: 0,
-          left: 24,
-        }}
-      > 
-        <XAxis dataKey="DeliveryDate" tickFormatter={(tick) => (new Date(tick).toLocaleDateString('ru'))}/>
-        <YAxis/>
-        <Tooltip
-            content={<CustomTooltip />} formatter={(value, _, props) => ([`${value}$ (${Math.round(props.payload.CostRUB*100)/100}₽)\n`])}
-        ></Tooltip>
-        <Area type="monotone" dataKey="CostUSD"/>
-      </AreaChart>
-    </ResponsiveContainer>
+    <div>
+      <Typography component="h2" variant="h6" align="right" mr={2} gutterBottom>
+        Всего сделано {total_orders} заказов на сумму {total_cost_usd}$ ({Math.round(total_cost_rub*100)/100})₽
+      </Typography>
+      <ResponsiveContainer width="100%" height={400}>
+        <AreaChart
+          data={stats}
+          margin={{
+            top: 16,
+            right: 16,
+            bottom: 0,
+            left: 24,
+          }}
+        > 
+          <XAxis dataKey="DeliveryDate" tickFormatter={(tick) => (new Date(tick).toLocaleDateString('ru'))}/>
+          <YAxis/>
+          <Tooltip
+              content={<CustomTooltip />} formatter={(value, _, props) => ([`${value}$ (${Math.round(props.payload.CostRUB*100)/100}₽)`])}
+          ></Tooltip>
+          <Area type="monotone" dataKey="CostUSD"/>
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
